968af9264541a95e0aafe514c1430cb8b7813724
Kevin Alberca <AwH@Turing.local>
2015-11-24 18:57:08 +0100

    Serveur node fonctionnel

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 /**
  * Created by AwH on 24/11/15.
  */
+
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+
+if(app.listen(3000)){
+    console.log('Application disponible sur le port 3000');
+} else {
+    console.log('L\'application n\'a pas pu etre demaree');
+}
