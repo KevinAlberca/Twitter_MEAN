a35f60f9a3cc544ca38b2bcf250fc4aec599a3ed
Kevin Alberca <AwH@Turing.local>
2015-11-24 21:05:21 +0100

    Edition du port d'ecoute

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+var port = process.env.PORT || 3000;
 
-if(app.listen(3000)){
-    console.log('Application disponible sur le port 3000');
+
+if(app.listen(port)){
+    console.log('Application disponible sur le port ' + port);
 } else {
     console.log('L\'application n\'a pas pu etre demaree');
 }
